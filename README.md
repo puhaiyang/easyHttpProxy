99392010f3f7770350ce351770d44d96571f410c
puhaiyang <puhaiyang@healthych.com>
2019-10-25 14:49:43 +0800

    1.init

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # easyHttpProxy
-support http/https proxy.类似于finddler,由java编写，代码简单便于理解。支持http/https代理！
+support http/https proxy.类似于fiddler,由java编写，代码简单便于理解。支持http/https代理！
---
+++ src/main/java/com/github/puhiayang/EasyHttpProxyServer.java
@@ -0,0 +1,10 @@
+package com.github.puhiayang;
+
+/**
+ * EasyHttpProxyServer
+ * created on 2019/10/25 14:44
+ *
+ * @author puhaiyang
+ */
+public class EasyHttpProxyServer {
+}
