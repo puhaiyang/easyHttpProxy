95650ef62677a7f6620525db279ab13c6692a74b
puhaiyang <puhaiyang@healthych.com>
2019-10-28 09:44:43 +0800

    1.添加socks代理方法

--- src/main/java/com/github/puhiayang/handler/proxy/HttpsProxyHandler.java
+++ src/main/java/com/github/puhiayang/handler/proxy/HttpsProxyHandler.java
@@ -5,13 +5,14 @@ import com.github.puhiayang.utils.HttpsSupport;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
 import io.netty.handler.ssl.SslContext;
 import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.util.Attribute;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 import static com.github.puhiayang.bean.Constans.CLIENTREQUEST_ATTRIBUTE_KEY;
 
@@ -25,22 +26,34 @@ public class HttpsProxyHandler extends ChannelInboundHandlerAdapter implements I
     private Logger logger = LoggerFactory.getLogger(HttpsProxyHandler.class);
 
     @Override
-    public void sendToServer(ClientRequest clientRequest, ChannelHandlerContext ctx, Object msg) {
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf byteBuf = (ByteBuf) msg;
         // ssl握手
         if (byteBuf.getByte(0) == 22) {
-            try {
-                Attribute<ClientRequest> clientRequestAttribute = ctx.channel().attr(CLIENTREQUEST_ATTRIBUTE_KEY);
-                SslContext sslCtx = SslContextBuilder
-                        .forServer(HttpsSupport.getInstance().getServerPriKey(), HttpsSupport.getInstance().getCert(clientRequestAttribute.get().getHost())).build();
-                ctx.pipeline().addFirst("httpCodec", new HttpServerCodec());
-                ctx.pipeline().addFirst("sslHandle", sslCtx.newHandler(ctx.alloc()));
-                // 重新过一遍pipeline，拿到解密后的的http报文
-                ctx.pipeline().fireChannelRead(msg);
-                return;
-            } catch (Exception e) {
-                logger.error("[sendToServer] err:{}", e.getMessage());
-            }
+            Attribute<ClientRequest> clientRequestAttribute = ctx.channel().attr(CLIENTREQUEST_ATTRIBUTE_KEY);
+            ClientRequest clientRequest = clientRequestAttribute.get();
+            sendToServer(clientRequest, ctx, msg);
+        }
+    }
+
+    @Override
+    public void sendToServer(ClientRequest clientRequest, ChannelHandlerContext ctx, Object msg) {
+        try {
+            SslContext sslCtx = SslContextBuilder
+                    .forServer(HttpsSupport.getInstance().getServerPriKey(), HttpsSupport.getInstance().getCert(clientRequest.getHost())).build();
+            //接收客户端请求，将客户端的请求内容解码
+            ctx.pipeline().addFirst("httpRequestDecoder", new HttpRequestDecoder());
+            //发送响应给客户端，并将发送内容编码
+            ctx.pipeline().addFirst("httpResponseEncoder", new HttpResponseEncoder());
+            //http聚合
+            ctx.pipeline().addLast("http-aggregator", new HttpObjectAggregator(65536));
+            //ssl处理
+            ctx.pipeline().addFirst("sslHandle", sslCtx.newHandler(ctx.alloc()));
+            // 重新过一遍pipeline，拿到解密后的的http报文
+            ctx.pipeline().fireChannelRead(msg);
+            return;
+        } catch (Exception e) {
+            logger.error("[sendToServer] err:{}", e.getMessage());
         }
     }
 }
--- src/main/java/com/github/puhiayang/handler/proxy/SocksProxyHandler.java
+++ src/main/java/com/github/puhiayang/handler/proxy/SocksProxyHandler.java
@@ -1,8 +1,11 @@
 package com.github.puhiayang.handler.proxy;
 
 import com.github.puhiayang.bean.ClientRequest;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.util.Attribute;
+
+import static com.github.puhiayang.bean.Constans.CLIENTREQUEST_ATTRIBUTE_KEY;
 
 
 /**
@@ -12,8 +15,49 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
  * created on 2019/10/25 20:56
  */
 public class SocksProxyHandler extends ChannelInboundHandlerAdapter implements IProxyHandler {
+    private ChannelFuture notHttpReuqstCf;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        Attribute<ClientRequest> clientRequestAttribute = ctx.channel().attr(CLIENTREQUEST_ATTRIBUTE_KEY);
+        ClientRequest clientRequest = clientRequestAttribute.get();
+        sendToServer(clientRequest, ctx, msg);
+    }
+
     @Override
     public void sendToServer(ClientRequest clientRequest, ChannelHandlerContext ctx, Object msg) {
-        //todo
+        //不是http请求就不管，全转发出去
+        if (notHttpReuqstCf == null) {
+            //连接至目标服务器
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(ctx.channel().eventLoop())
+                    // 复用客户端连接线程池
+                    .channel(ctx.channel().getClass())
+                    // 使用NioSocketChannel来作为连接用的channel类
+                    .handler(new ChannelInitializer() {
+                        @Override
+                        protected void initChannel(Channel ch) throws Exception {
+                            ch.pipeline().addLast(new ChannelInboundHandlerAdapter() {
+                                @Override
+                                public void channelRead(ChannelHandlerContext ctx0, Object msg) throws Exception {
+                                    ctx.channel().writeAndFlush(msg);
+                                }
+                            });
+                        }
+                    });
+            notHttpReuqstCf = bootstrap.connect(clientRequest.getHost(), clientRequest.getPort());
+            notHttpReuqstCf.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    if (future.isSuccess()) {
+                        future.channel().writeAndFlush(msg);
+                    } else {
+                        ctx.channel().close();
+                    }
+                }
+            });
+        } else {
+            notHttpReuqstCf.channel().writeAndFlush(msg);
+        }
     }
 }
